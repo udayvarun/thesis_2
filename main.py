d02a6f41e0fcb235eb815b0640dbb54d27619faf
Uday Varun Kothapalli <uday.varun.kothapalli@fau.de>
2024-08-05 15:06:08 +0200

    updated functions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PCA import PCA
 import numpy as np
 from dataset_manipulator import *
-from map_to_robot import map_to_robot
 from dataset_extractor import DataSet
+from robot_synergy_input import *
 
 folder = r".\DataSets\two_waves"
 # folder = r".\DataSets\hand_to_mouth"
@@ -11,16 +11,13 @@ folder = r".\DataSets\two_waves"
 # side = "left"
 side = "right"
 
-# data = DataSet(folder, side).dataset_pca()
-# DataSet(folder, side).plot_data()
-
 data = DataSet(folder, side).single_dataset()[0]
 
 # Manipulate joint angles
 data_reconstructed = dataset_manipulator_without_offset(data)
 data_offset = dataset_manipulator_with_offset(data_reconstructed)
 
-n_components = 1
+n_components = 3
 pca = PCA(data_offset, n_components)
 
 # Print Original data with joint angles
@@ -34,15 +31,5 @@ pca.pca_plot()
 pca.print_pca_data()
 print(pca.get_pca_components())
 
-# Calculation of inverse PCA
-inverse_result = pca.pca_inverse_calculation()
-pca.inverse_pca_plot()
-
-# Print Reconstructed data with joint angles
-pca.print_reconstructed_data()
-
-# Map original data
-# map_to_robot(data)
-
-# Map inverse PCA data
-map_to_robot(inverse_result)
+q_optimal = minimized(pca_components)
+print(q_optimal)
--- robot_synergy_input.py
+++ robot_synergy_input.py
@@ -32,70 +32,72 @@ def start_control_constraint(u):
     u = u.reshape(T, nu)
     return u[0]
 
+def minimized(S):
+    ## Parameters
+    T = 100  # Steps in time horizon
+    nu = 3 # Number of PCA components used
+    nq = 7 # Number of robot joints
 
-## Parameters
-T = 100  # Steps in time horizon
-nu = 3 # Number of PCA components used
-nq = 7 # Number of robot joints
+    # Initial joint position
+    q0 = np.array([1.0, 0.5, 0.6, 0.4, 0.3, 0.1, 0.0])  
 
-# Initial joint position
-q0 = np.array([1.0, 0.5, 0.6, 0.4, 0.3, 0.1, 0.0])  
+    # Synergy matrix: Contains PCA components
+    # S = np.array([[1.0,1.0,1.0,1.0,1.0,1.0,1.0,],[0.1,0.2,0.3,0.4,0.5,0.6,0.7],[-0.5,0.5,-0.5,-0.5,-0.5,-0.5,-0.5]])
+    S = np.transpose(S)
 
-# Synergy matrix: Contains PCA components
-S = np.array([[1.0,1.0,1.0,1.0,1.0,1.0,1.0,],[0.1,0.2,0.3,0.4,0.5,0.6,0.7],[-0.5,0.5,-0.5,-0.5,-0.5,-0.5,-0.5]])
-S = np.transpose(S)
+    # Initial guess for the control inputs
+    u_initial = np.zeros((T, nu))
 
-# Initial guess for the control inputs
-u_initial = np.zeros((T, nu))
-
-# Desired goal position in joint space
-q_goal = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
-
-
-## Constraints (first and last control input should be zero, rate of change constraint)
-constraints = [
-    {'type': 'eq', 'fun': start_control_constraint},
-    {'type': 'eq', 'fun': end_control_constraint}
-]
+    # Desired goal position in joint space
+    q_goal = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
 
 
-## Optimize
-result = minimize(cost_function, u_initial, constraints=constraints)
-
-# Optimal control inputs and states (recreate the joint trajectories)
-u_optimal = result.x.reshape(T, nu)
-q_optimal = np.zeros((T+1, nq))
-q_optimal[0] = q0
-for t in range(T):
-    q_optimal[t+1] = q_optimal[t] + np.matmul(S, u_optimal[t])
+    ## Constraints (first and last control input should be zero, rate of change constraint)
+    constraints = [
+        {'type': 'eq', 'fun': start_control_constraint},
+        {'type': 'eq', 'fun': end_control_constraint}
+    ]
 
 
-## Plot the results
-time = np.arange(T+1)
+    ## Optimize
+    result = minimize(cost_function, u_initial, constraints=constraints)
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 10))
+    # Optimal control inputs and states (recreate the joint trajectories)
+    u_optimal = result.x.reshape(T, nu)
+    q_optimal = np.zeros((T+1, nq))
+    q_optimal[0] = q0
+    for t in range(T):
+        q_optimal[t+1] = q_optimal[t] + np.matmul(S, u_optimal[t])
 
-# Plot für die Zustandsvariablen
-for i in range(q_optimal.shape[1]):
-    ax1.plot(time, q_optimal[:, i], label=f'State {i+1}')
-    ax1.set_xlabel('Time')
-    ax1.set_ylabel('State Variables')
-    ax1.legend()
-    ax1.grid(True)
-    ax1.set_title('State Variables over Time')
 
-# Plot für die Steuerungsvariablen
-for i in range(nu):
-    if nu > 1:
-        ax2.plot(time[:-1], u_optimal[:, i], label=f'Control {i+1}')
-    else:
-        ax2.plot(time[:-1], u_optimal, label=f'Control {i+1}')
-    ax2.set_xlabel('Time')
-    ax2.set_ylabel('Control Variables')
-    ax2.legend()
-    ax2.grid(True)
-    ax2.set_title('Control Variables over Time')
+    ## Plot the results
+    time = np.arange(T+1)
 
-plt.tight_layout()
-plt.show()
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 10))
+
+    # Plot für die Zustandsvariablen
+    for i in range(q_optimal.shape[1]):
+        ax1.plot(time, q_optimal[:, i], label=f'State {i+1}')
+        ax1.set_xlabel('Time')
+        ax1.set_ylabel('State Variables')
+        ax1.legend()
+        ax1.grid(True)
+        ax1.set_title('State Variables over Time')
+
+    # Plot für die Steuerungsvariablen
+    for i in range(nu):
+        if nu > 1:
+            ax2.plot(time[:-1], u_optimal[:, i], label=f'Control {i+1}')
+        else:
+            ax2.plot(time[:-1], u_optimal, label=f'Control {i+1}')
+        ax2.set_xlabel('Time')
+        ax2.set_ylabel('Control Variables')
+        ax2.legend()
+        ax2.grid(True)
+        ax2.set_title('Control Variables over Time')
+
+    plt.tight_layout()
+    plt.show()
+
+    return q_optimal
 
