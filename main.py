a0bb46abf9839e11bf987691f19a69802ea5c0c5
Uday Varun Kothapalli <uday.varun.kothapalli@fau.de>
2024-08-08 11:23:07 +0200

    pca trained with 3 experiments

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 from PCA import PCA
-import numpy as np
 from dataset_manipulator import *
 from dataset_extractor import DataSet
-from robot_synergy_input import *
+from robot_synergy_input import minimize_dataset
+from map_to_robot import map_to_robot
 
-folder = r".\DataSets\two_waves"
-# folder = r".\DataSets\hand_to_mouth"
-# folder = r".\DataSets\hand_through_hair_right_arm"
+folders = []
+folders.append(r".\DataSets\two_waves")
+folders.append(r".\DataSets\hand_to_mouth")
+folders.append(r".\DataSets\hand_through_hair_right_arm")
 
-# side = "left"
-side = "right"
+data = []
+for folder in folders:
+    data.extend(DataSet(folder, "right").single_dataset()[0])
 
-data = DataSet(folder, side).single_dataset()[0]
+data = np.array(data)
+dataset_manipulator_without_offset(data)
 
-# Manipulate joint angles
-data_reconstructed = dataset_manipulator_without_offset(data)
-data_offset = dataset_manipulator_with_offset(data_reconstructed)
-
-n_components = 3
-pca = PCA(data_offset, n_components)
-
-# Print Original data with joint angles
-pca.print_original_data()
+n_components = 7
+pca = PCA(data, n_components)
 
 # Calculation of PCA
 pca_components = pca.pca_calculation()
-pca.pca_plot()
 
-# Print PCA data
-pca.print_pca_data()
 print(pca.get_pca_components())
 
-q_optimal = minimized(pca_components)
-print(q_optimal)
+min = minimize_dataset(pca_components)
+
+initial_position =  np.array([1.0, 0.5, 0.6, 0.4, 0.3, 0.1, 0.0]) 
+goal_position = np.array([1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2])
+
+q_optimal = min.minimize_function(initial_position, goal_position)
+min.plot_variables()
+
+dataset_manipulator_without_offset(q_optimal)
+
+map_to_robot(q_optimal)
