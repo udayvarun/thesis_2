37ecfc6771fd05ef28f7f313e1ea48f06af4ae23
Uday Varun Kothapalli <uday.varun.kothapalli@fau.de>
2024-08-08 11:51:28 +0200

    reusing the results from a file

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
-from PCA import PCA
-from dataset_manipulator import *
-from dataset_extractor import DataSet
 from robot_synergy_input import minimize_dataset
 from map_to_robot import map_to_robot
+import pickle as pk
+import numpy as np
+from dataset_manipulator import *
 
-folders = []
-folders.append(r".\DataSets\two_waves")
-folders.append(r".\DataSets\hand_to_mouth")
-folders.append(r".\DataSets\hand_through_hair_right_arm")
-
-data = []
-for folder in folders:
-    data.extend(DataSet(folder, "right").single_dataset()[0])
-
-data = np.array(data)
-dataset_manipulator_without_offset(data)
-
-n_components = 7
-pca = PCA(data, n_components)
-
-# Calculation of PCA
-pca_components = pca.pca_calculation()
-
-print(pca.get_pca_components())
+pca_reload = pk.load(open("pca.pkl",'rb'))
+pca_components = pca_reload.pca.components_
 
 min = minimize_dataset(pca_components)
 
