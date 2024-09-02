6c36747d5f0ef6190529210f1e0b50b51f93901e
Uday Varun Kothapalli <uday.varun.kothapalli@fau.de>
2024-09-02 11:17:31 +0200

    Added more experiments

--- PCA_training.py
+++ PCA_training.py
@@ -7,6 +7,8 @@ folders = []
 folders.append(r".\DataSets\two_waves")
 folders.append(r".\DataSets\hand_to_mouth")
 folders.append(r".\DataSets\hand_through_hair_right_arm")
+folders.append(r".\DataSets\files_motions_986")
+folders.append(r".\DataSets\take_book_from_shelf")
 
 data = []
 for folder in folders:
