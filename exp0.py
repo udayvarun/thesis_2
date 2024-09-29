8f53077280d6eae25a41a543826b74cf137ec7c3
Uday Varun Kothapalli <uday.varun.kothapalli@fau.de>
2024-09-29 21:15:19 +0200

    added initial position hard code

--- exp0.py
+++ exp0.py
@@ -13,6 +13,7 @@ min = minimize_dataset(pca_components, 50)
 Panda().move_to_start()
 
 initial_position = Panda().get_robot_position()
+# initial_position = np.array([-0.00867367069998331, -0.7859290811555426, 0.0014189809690487837, -2.356400179186334, 0.003559221795097906, 1.5683314154592496, 0.7770764079358842])
 
 #
 #goal_position = np.array([[0.2091, 0.1494, 0.9664, 0.9337],
