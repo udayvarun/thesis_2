39b5516a9c42897b6042ac1a512cadf8735c025c
Uday Varun Kothapalli <uday.varun.kothapalli@fau.de>
2024-10-13 19:59:00 +0200

    added initial position sample

--- exp0.py
+++ exp0.py
@@ -7,6 +7,7 @@ from utils.calculate_q import calculate_q
 Panda().move_to_start()
 
 initial_position = Panda().get_robot_position()
+# initial_position = np.array([-0.00867367069998331, -0.7859290811555426, 0.0014189809690487837, -2.356400179186334, 0.003559221795097906, 1.5683314154592496, 0.7770764079358842])
 goal_position = np.array([[<desired_matrix>],
                           [0, 0, 0, 1]])
 calculate_q(initial_position, goal_position, 'exp0_1', 10)
