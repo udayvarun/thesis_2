18db885a9c43127f5c73f87d82472761ebb7e1d5
Uday Varun Kothapalli <uday.varun.kothapalli@fau.de>
2024-10-18 09:09:16 +0200

    simulation execution

--- utils/map_to_robot_simulation.py
+++ utils/map_to_robot_simulation.py
@@ -1,14 +1,18 @@
 import roboticstoolbox as rtb
 import swift
+import numpy as np
+from utils.dataset_extractor import DataSet
 
-def map_to_robot(data):
+def map_to_robot(data, time):
+    q_array = np.load(f'./experiment_data_set/{data}.npy')
+    DataSet.joint_angles_plot(q_array)
     env = swift.Swift()
     env.launch(realtime=True)
     panda = rtb.models.Panda()
     env.add(panda)
     dt = 0.01
-    while True:
-        for q in data:
-            panda.q = q
-            env.step(dt)
-    env.hold()
+    for q in q_array:
+        panda.q = q
+        env.step(dt)
+        print(q)
+    # env.hold()
