d76381f3841218ffa915e664da3ac0fc9a707ea1
Uday Varun Kothapalli <uday.varun.kothapalli@fau.de>
2024-11-15 20:18:25 +0100

    simulation files fix

--- simulation_exp/exp3_1_simulation.py
+++ simulation_exp/exp3_1_simulation.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import numpy as np
 from utils.map_to_robot_simulation import map_to_robot
 import time
