8b8176b693edbcf0ae9d2abdcfcb2b60e053cb51
Uday Varun Kothapalli <uday.varun.kothapalli@fau.de>
2024-11-18 19:57:03 +0100

    exp3_control

--- exp3_1.py
+++ exp3_1.py
@@ -1,20 +1,22 @@
 import numpy as np
-from utils.real_robot_mapper import Panda
-from utils.move_position import move_to_position
+# from utils.real_robot_mapper import Panda
+# from utils.move_position import move_to_position
 import time
 from utils.calculate_q import calculate_q
 
-goal_joint_position = np.array([0.14657515533891577, 1.5838447781278373, -1.5818225305216425, -0.38852626337635504, 1.5798191279371578, 3.1866216808557506, 0.8204606715504958])
-Panda().move_to_joint_position(goal_joint_position)
+initial_position = np.array([0.14657515533891577, 1.5838447781278373, -1.5818225305216425, -0.38852626337635504, 1.5798191279371578, 3.1866216808557506, 0.8204606715504958])
+# Panda().move_to_joint_position(goal_joint_position)
+
+filepath = r"experiment_data_set\figures\exp3_control_and_state_variables.png"
 
 #initial_position = Panda().get_robot_position()
-#goal_position = np.array([[0.0305, -0.9993, 0.02121, 0.6728],
-#                    [0.9765, 0.02526, -0.2141, 0.05834],
-#                    [0.2134, 0.02724, 0.9766, 0.8438],
-#                    [0, 0, 0, 1]])
-#calculate_q(initial_position, goal_position, 'exp3_1_1', 3, factor = 10)
+goal_position = np.array([[0.0305, -0.9993, 0.02121, 0.6728],
+                   [0.9765, 0.02526, -0.2141, 0.05834],
+                   [0.2134, 0.02724, 0.9766, 0.8438],
+                   [0, 0, 0, 1]])
+calculate_q(initial_position, goal_position, 'exp3_1_1', 3, filepath, factor = 10)
 
-move_to_position('exp3_1_1', 3)
+# move_to_position('exp3_1_1', 3)
 
 time.sleep(2)
 
@@ -25,4 +27,4 @@ time.sleep(2)
 #                         [0, 0, 0, 1]])
 #calculate_q(initial_position, goal_position, 'exp3_1_2', 3, factor = 10)
 
-move_to_position('exp3_1_2', 3)
+# move_to_position('exp3_1_2', 3)
